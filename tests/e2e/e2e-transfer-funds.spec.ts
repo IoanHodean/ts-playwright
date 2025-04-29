278ab0bae1ab0cb4abf132c251fcf26d8d3079fa
IoanHodean <hodean.ioan@gmail.com>
2025-04-29 10:05:31 +0300

    refactor: streamline login process in transfer funds test

--- tests/e2e/e2e-transfer-funds.spec.ts
+++ tests/e2e/e2e-transfer-funds.spec.ts
@@ -4,8 +4,7 @@ import { baseURL } from '../../playwright.config';
 test.describe.parallel("Transfer Funds and Make Payments", () => {
     //Before hook
     test.beforeEach(async ({ page }) => {
-        await page.goto(baseURL+'/login.html');
-        await page.click(`#signin_button`);
+        await page.goto(baseURL+'/login.html');       
         await page.locator('#user_login').fill('username');
         await page.locator('#user_password').fill('password');
         await page.locator('.btn-primary').click();
@@ -13,5 +12,13 @@ test.describe.parallel("Transfer Funds and Make Payments", () => {
         await expect(page.url()).toContain('/bank/transfer-funds.html'); 
 
     })
-
-    })
+    test('Transfer Funds', async ({ page }) => {
+        await page.locator('#tf_fromAccountId').selectOption('2'); // Checking account
+        await page.locator('#tf_toAccountId').selectOption('3'); // Savings account
+        await page.locator('#tf_amount').fill('1000');
+        await page.locator('#tf_description').fill('Test transfer funds');
+        await page.locator('#btn_submit').click();
+        await page.locator('#btn_submit').click();
+        await expect(page.locator('.alert')).toContainText('You successfully submitted your transaction.');
+        })
+})
