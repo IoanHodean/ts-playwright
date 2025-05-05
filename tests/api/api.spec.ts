8e0ae7d7328347cda4b7630f09fd086b54888995
IoanHodean <hodean.ioan@gmail.com>
2025-05-05 09:18:28 +0300

    Update API configuration and add comprehensive API tests fixed expected values in tests/api/api.spec.ts

---
+++ tests/api/api.spec.ts
@@ -0,0 +1,115 @@
+import {test, expect, request} from '@playwright/test';
+import { baseURL } from '../../api.config';
+
+
+test.describe.parallel('API Tests', () => {      
+
+test('GET request for list', async ({request}) => {
+    const response= await request.get(`${baseURL}/users?page=2`);
+    expect (response.status()).toBe(200);
+    const responseBody = await response.json();
+    
+
+});
+test ('Get request with invalid endpoint', async ({request}) => {
+    const response= await request.get(`${baseURL}/users/nonexistent`);
+    expect (response.status()).toBe(404);
+});
+test ('Get request of single user', async ({request}) => {
+    const response= await request.get(`${baseURL}/users/2`);    
+    expect (response.status()).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody.data.id).toBeTruthy();
+    expect(responseBody.data.first_name).toBeTruthy();
+    expect(responseBody.data.last_name).toBeTruthy();
+    expect(responseBody.data.avatar).toBeTruthy();
+    expect(responseBody.data.email).toBeTruthy();   
+});
+test ('validate all users in list', async ({request}) => {
+    const response= await request.get(`${baseURL}/users?page=2`);
+    expect (response.status()).toBe(200);
+    const responseBody = await response.json();
+    for (let i = 0; i < responseBody.data.length; i++) {
+        expect(responseBody.data[i].id).toBeTruthy();
+        expect(responseBody.data[i].first_name).toBeTruthy();
+        expect(responseBody.data[i].last_name).toBeTruthy();
+        expect(responseBody.data[i].avatar).toBeTruthy();
+        expect(responseBody.data[i].email).toBeTruthy(); 
+    }
+
+});
+test ('POST request to create user', async ({request}) => {
+    const response= await request.post(`${baseURL}/users`, {
+        data: {
+            name: 'John Doe',
+            job: 'Software Engineer'
+        }
+    });
+    expect (response.status()).toBe(201);
+    const responseBody = await response.json();
+    expect(responseBody.name).toBe('John Doe');
+    expect(responseBody.job).toBe('Software Engineer');
+});
+test ('POST request to create user with falsy job', async ({request}) => {
+    const response= await request.post(`${baseURL}/users`, {
+        data: {
+            name: 'John Doe',
+            job: ''
+        }
+    });
+    expect (response.status()).toBe(400);
+    const responseBody = await response.json();
+    expect(responseBody.error).toBe('Missing job title');
+});
+test ('POST request to create user with falsy name', async ({request}) => {
+    const response= await request.post(`${baseURL}/users`, {
+        data: {
+            name: '',
+            job: 'general manager'
+        }
+    });
+    expect (response.status()).toBe(400);
+    const responseBody = await response.json();
+    expect(responseBody.error).toBe('Missing name');
+});
+test('POST request login', async ({request}) => {
+    const response= await request.post(`${baseURL}/login`, {
+        data: {
+            email: 'eve.holt@reqres.in',
+            password: 'cityslicka'
+        }
+    });
+    expect (response.status()).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody.token).toBeTruthy();
+});
+test ('PUT request to update user', async ({request}) => {
+    const response= await request.put(`${baseURL}/users/2`, {
+        data: {
+            name: 'John Doe',
+            job: 'Senior Software Engineer'
+        }
+    });
+    expect (response.status()).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody.name).toBe('John Doe');
+    expect(responseBody.job).toBe('Senior Software Engineer');        
+});
+test ('PATCH request to update user', async ({request}) => {
+    const response= await request.patch(`${baseURL}/users/2`, {
+        data: {
+            name: 'John Doe',
+            job: 'Senior Software Engineer'
+        }
+    });
+    expect (response.status()).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody.name).toBe('John Doe');
+    expect(responseBody.job).toBe('Senior Software Engineer');        
+
+});
+test ('DELETE request to delete user', async ({request}) => {
+    const response= await request.delete(`${baseURL}/users/2`);
+    expect (response.status()).toBe(204);        
+});
+});
